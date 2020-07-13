5922318850cc246fa86cbe1ebb34c5412f790f6f
Verbitsky Serhey <training.v.sergey@gmail.com>
2020-07-13 15:07:47 +0300

    add javadoc comments to Crawler.java

--- src/main/java/beans/crawler/Crawler.java
+++ src/main/java/beans/crawler/Crawler.java
@@ -1,15 +1,48 @@
 package beans.crawler;
 
+/**
+ * @author Verbitsky Segey
+ * Base interfece of Web Crawler
+ * Provides methods for control web crawling process
+ * @version 1.0
+ */
+
 public interface Crawler {
+    //Start crawling process
     void startCrawl();
 
+    //Stop crawling process
     void stopCrawl();
 
+    //Stop crawling process when get an error
     void stopCrawlWithCrash();
+
+    /**
+     * @return boolean value "true" if
+     * an error occurred during crawling processing
+     * This will end the crawling process
+     */
 
     boolean getErrorStatus();
 
+    /**
+     * Display site statistic, that contains some information
+     * about the number of processed sites, the number of words found,
+     *
+     * @see beans.statistic.StatisticResult
+     * @see beans.statistic.StatisticManager
+     */
+
     void showStatistic();
+
+    /**
+     * Save statistic to file
+     * @param fileName - contains the specific file name
+     *                 for creating the statistic file
+     *
+     * @see beans.statistic.StatisticResult
+     * @see beans.statistic.StatisticManager
+     */
 
     void saveStatistic(String fileName);
 }
